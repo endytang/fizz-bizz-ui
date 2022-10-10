ea44c8b69c9f31d4464ead3a82efc99a2d4503b9
Endy Tang <endytang0310@gmail.com>
2022-10-10 23:10:30 +0100

    update

--- main.js
+++ main.js
@@ -1,13 +1,39 @@
-console.log("hello world")
-var mainbutton = document.getElementById("main-button")
-mainbutton.onclick=function (){
+var mainbutton = document.getElementById("main-button");
+mainbutton.onclick = function (){
   // document.getElementsByTagName("h1")[0].classList.add("hidden")
-  var h1 = document.getElementsByTagName("h1")[0];
-  if(h1.classList.contains("hidden")){
-    h1.classList.remove("hidden")
-    mainbutton.innerText="hide title"
+  var div = document.getElementsByClassName("hidden-button")[0];
+
+  if(div.classList.contains("hidden")){
+    mainbutton.classList.add("hidden");
+    div.classList.remove("hidden");
   }else{
-    h1.classList.add("hidden")
-    mainbutton.innerText="show title"
+    div.classList.add("hidden");
+    mainbutton.classList.remove("main-button");
+    }
+}
+
+var restart = document.getElementById("restart");
+restart.onclick = function(){
+
+  var div = document.getElementsByClassName("hidden-button")[0];
+
+  if(mainbutton.classList.contains("hidden")){
+    div.classList.add("hidden")
+    mainbutton.classList.remove("hidden")
+}else{
+    mainbutton.classList.remove("hidden");
+    div.classList.remove("hidden")
   }
 }
+
+
+const Number = 100;
+const arr = Array.from({length: Number}, (_, index) => index + 1);
+console.log(arr);
+
+// if( arr == 3){
+//   console.log("its 3")
+// }else {
+//   console.log("its not 3")
+// }
+
