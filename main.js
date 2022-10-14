4f8ff6bb6ba377d5e6ab3cb5cf65b53033f81b6b
Endy Tang <endytang0310@gmail.com>
2022-10-14 15:35:45 +0100

    correct change

--- main.js
+++ main.js
@@ -1,35 +1,17 @@
 var mainbutton = document.getElementById("main-button");
 mainbutton.onclick = function (){
-  // document.getElementsByTagName("h1")[0].classList.add("hidden")
   var button = document.querySelector("#play-button");
 
   if(!button.classList.contains("reset")){
     button.innerHTML="<i class=\"fa fa-refresh fa-spin\" style=\"font-size:24px\"></i> Reset"
     button.classList.add("reset")
-    document.getElementById("fizzbizz").innerHTML=fizzbizzstring
+    document.getElementById("fizzbizz").innerHTML=fizzbizz.join(", ")
   }else{
     button.innerHTML="<i style=\"font-size:24px\" class=\"fa\">&#xf04b;</i> Play"
     button.classList.remove("reset")
-    document.getElementById("fizzbizz").innerHTML=arr.join(",  ")
+    document.getElementById("fizzbizz").innerHTML=arr.join(", ")
     }
 }
-
-// var restart = document.getElementById("restart");
-// restart.onclick = function(){
-//
-//   var div = document.getElementsByClassName("hidden-button")[0];
-//
-//   if(mainbutton.classList.contains("hidden")){
-//     div.classList.add("hidden")
-//     mainbutton.classList.remove("hidden")
-//
-// }else{
-//     mainbutton.classList.remove("hidden");
-//     div.classList.remove("hidden")
-//
-//   }
-// }
-
 
 const Number = 100;
 const arr = Array.from({length: Number}, (_, index) => index + 1);
@@ -44,6 +26,4 @@ const fizzbizz = arr.map(function(originalNumber,index) {
   return originalNumber;
 })
 
-const fizzbizzstring = fizzbizz.toString()
-console.log(fizzbizzstring)
 document.getElementById("fizzbizz").innerHTML=arr.join(", ")
