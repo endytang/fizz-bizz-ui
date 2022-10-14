47584f18c61de057c70bd50f0b0650edc33db7f9
endytang <110055801+endytang@users.noreply.github.com>
2022-10-14 15:37:12 +0100

    Increase the font and color the text (#5)
    * Increase the font and color the text
    
    * change color
    
    * correct change

--- main.js
+++ main.js
@@ -1,42 +1,29 @@
 var mainbutton = document.getElementById("main-button");
 mainbutton.onclick = function (){
-  // document.getElementsByTagName("h1")[0].classList.add("hidden")
-  var div = document.getElementsByClassName("hidden-button")[0];
+  var button = document.querySelector("#play-button");
 
-  if(div.classList.contains("hidden")){
-    mainbutton.classList.add("hidden");
-    div.classList.remove("hidden");
+  if(!button.classList.contains("reset")){
+    button.innerHTML="<i class=\"fa fa-refresh fa-spin\" style=\"font-size:24px\"></i> Reset"
+    button.classList.add("reset")
+    document.getElementById("fizzbizz").innerHTML=fizzbizz.join(", ")
   }else{
-    div.classList.add("hidden");
-    mainbutton.classList.remove("main-button");
+    button.innerHTML="<i style=\"font-size:24px\" class=\"fa\">&#xf04b;</i> Play"
+    button.classList.remove("reset")
+    document.getElementById("fizzbizz").innerHTML=arr.join(", ")
     }
 }
-
-var restart = document.getElementById("restart");
-restart.onclick = function(){
-
-  var div = document.getElementsByClassName("hidden-button")[0];
-
-  if(mainbutton.classList.contains("hidden")){
-    div.classList.add("hidden")
-    mainbutton.classList.remove("hidden")
-}else{
-    mainbutton.classList.remove("hidden");
-    div.classList.remove("hidden")
-  }
-}
-
 
 const Number = 100;
 const arr = Array.from({length: Number}, (_, index) => index + 1);
 const fizzbizz = arr.map(function(originalNumber,index) {
   if (originalNumber % 5 == 0 && originalNumber % 3 == 0 ) {
-    return "Fizzbizz"
+    return "<b><span class='text-red'>FizzBizz</span></b>";
   } else if (originalNumber % 3 == 0) {
-    return "Fizz"
+    return "<span class='text-blue'>Fizz</span>";
   } else if ( originalNumber % 5 == 0) {
-    return "Bizz";
+    return "<span class='text-green'>Bizz</span>";
   }
   return originalNumber;
 })
-console.log(fizzbizz);
+
+document.getElementById("fizzbizz").innerHTML=arr.join(", ")
