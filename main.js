7d4f1a19f2cc75f2a2a8c41befcd2d89c7c86339
Endy Tang <endytang0310@gmail.com>
2022-10-25 23:07:04 +0100

    reverse button

--- main.js
+++ main.js
@@ -1,17 +1,41 @@
-var playbutton = document.getElementById("play-button");
-var pausebutton = document.getElementById("pause-button")
+var playButton = document.getElementById("play-button");
+var pauseButton = document.getElementById("pause-button")
 var fizzbizzElement = document.getElementById("fizzbizz")
-var switchbutton = document.getElementById("switch-button")
+var switchButton = document.getElementById("switch-button")
 
-playbutton.onclick = function (){
+function getFizzBizzString(number) {
+  const arr = Array.from({length: number}, (_, index) => index + 1);
+  const fizzbizz = arr.map(function(originalNumber,index) {
+    if (originalNumber % 5 == 0 && originalNumber % 3 == 0 ) {
+      return "<b><span class='text-red'>FizzBizz</span></b>";
+    } else if (originalNumber % 3 == 0) {
+      return "<span class='text-blue'>Fizzbb</span>";
+    } else if ( originalNumber % 5 == 0) {
+      return "<span class='text-green'>Bizz</span>";
+    }
+    return originalNumber;
+  })
+
+  return fizzbizz.join(', ')
+}
+
+
+function getArrString(number){
+  const arr = Array.from({length: number}, (_, index) => index + 1);
+
+  return arr.join(", ")
+}
+
+playButton.onclick = function (){
   var button = document.querySelector("#play-button");
+  var currNumber = document.getElementById("newvalue").value;
 
   if(!button.classList.contains("Switch")){
     // press play
     button.innerHTML="<i class=\"fa fa-refresh fa-spin\" style=\"font-size:24px\"></i> Reset"
     button.classList.add("Switch")
-    pausebutton.classList.remove("hidden")
-    fizzbizzElement.innerHTML=fizzbizz.join(", ")
+    pauseButton.classList.remove("hidden")
+    fizzbizzElement.innerHTML=getFizzBizzString(currNumber)
     fizzbizzElement.classList.add("animation")
     fizzbizzElement.classList.remove("animapaused")
 
@@ -20,23 +44,24 @@ playbutton.onclick = function (){
     button.innerHTML="<i style=\"font-size:24px\" class=\"fa\">&#xf04b;</i> Play"
     fizzbizzElement.classList.remove("animation")
     button.classList.remove("Switch")
-    pausebutton.classList.add("hidden")
-    fizzbizzElement.innerHTML=arr.join(", ")
+    pauseButton.classList.add("hidden")
+    fizzbizzElement.innerHTML=getArrString(currNumber)
 
   }
 }
 
-switchbutton.onclick = function() {
 
+switchButton.onclick = function() {
+  var currNumber = document.getElementById("newvalue").value;
   if ( fizzbizzElement.innerText.includes("Fizz")) {
-    fizzbizzElement.innerHTML = arr.join(", ")
+    fizzbizzElement.innerHTML = getArrString(currNumber)
   } else {
-    fizzbizzElement.innerHTML = fizzbizz.join(", ")
+    fizzbizzElement.innerHTML = getFizzBizzString(currNumber)
   }
 }
 
 
-pausebutton.onclick = function (){
+pauseButton.onclick = function (){
   var button2 = document.querySelector("#pause-button");
   if(fizzbizzElement.classList.contains("animapaused")){
     button2.innerHTML="<i style=\"font-size:24px\" class=\"fa\">&#xf04c</i> Pause"
@@ -47,19 +72,17 @@ pausebutton.onclick = function (){
   }
 }
 
-
-
-const Number = 100;
-const arr = Array.from({length: Number}, (_, index) => index + 1);
-const fizzbizz = arr.map(function(originalNumber,index) {
-  if (originalNumber % 5 == 0 && originalNumber % 3 == 0 ) {
-    return "<b><span class='text-red'>FizzBizz</span></b>";
-  } else if (originalNumber % 3 == 0) {
-    return "<span class='text-blue'>Fizz</span>";
-  } else if ( originalNumber % 5 == 0) {
-    return "<span class='text-green'>Bizz</span>";
+var reverseButton = document.getElementById("animation-reverse")
+reverseButton.onclick = function (){
+  if(!fizzbizzElement.classList.contains("animationreverse")){
+    fizzbizzElement.classList.add("animationreverse")
+  }else{
+    fizzbizzElement.classList.remove("animationreverse")
   }
-  return originalNumber;
-})
+}
 
-// document.getElementById("fizzbizz").innerHTML=arr.join(", ")
+function getNewCombo() {
+  var newNumber = document.getElementById("newvalue").value;
+  var fizzbizz = getArrString(newNumber)
+  fizzbizzElement.innerHTML = fizzbizz
+}
