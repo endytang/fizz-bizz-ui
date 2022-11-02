48930cdf9c581f351ba090da81988318a84721e6
Endy Tang <endytang0310@gmail.com>
2022-11-02 18:03:57 +0000

    move button

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
-var mainbutton = document.getElementById("main-button");
-mainbutton.onclick = function (){
+var playbutton = document.getElementById("play-button");
+
+var fizzbizztag = document.getElementById("fizzbizz")
+
+playbutton.onclick = function (){
   var button = document.querySelector("#play-button");
 
   if(!button.classList.contains("reset")){
+    console.log("test1")
     button.innerHTML="<i class=\"fa fa-refresh fa-spin\" style=\"font-size:24px\"></i> Reset"
     button.classList.add("reset")
-    document.getElementById("fizzbizz").innerHTML=fizzbizz.join(", ")
+    fizzbizztag.innerHTML=fizzbizz.join(", ")
   }else{
+    console.log("test2")
     button.innerHTML="<i style=\"font-size:24px\" class=\"fa\">&#xf04b;</i> Play"
     button.classList.remove("reset")
-    document.getElementById("fizzbizz").innerHTML=arr.join(", ")
+    fizzbizztag.innerHTML=numbers.join(", ")
     }
 }
 
+
+
+
 const Number = 100;
 const arr = Array.from({length: Number}, (_, index) => index + 1);
+const numbers = arr.map((_, index) => `<span>${index + 1}</span>`)
 const fizzbizz = arr.map(function(originalNumber,index) {
   if (originalNumber % 5 == 0 && originalNumber % 3 == 0 ) {
     return "<b><span class='text-red'>FizzBizz</span></b>";
@@ -26,4 +35,27 @@ const fizzbizz = arr.map(function(originalNumber,index) {
   return originalNumber;
 })
 
-document.getElementById("fizzbizz").innerHTML=arr.join(", ")
+document.getElementById("fizzbizz").innerHTML=numbers.join(", ")
+
+
+var move = document.getElementById("move-button")
+var leftposition = 0;
+
+let   intervalfunction
+
+move.onclick = function(){
+
+  if(!move.classList.contains("reset")){
+  move.classList.add("reset")
+    move.innerHTML="Reset"
+     intervalfunction = setInterval(function (){
+      leftposition = leftposition+1
+      fizzbizztag.style.left = `-${leftposition}px`
+    },25)
+  }else{
+    move.classList.remove("reset")
+    move.innerHTML="move"
+    clearInterval(intervalfunction)
+  }
+
+}
